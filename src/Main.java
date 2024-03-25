8793e569829310ccddaa889375b9cc51efdbb616
ElenaBer77 <pei281277@gmail.com>
2024-03-25 18:31:52 +0300

    name

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String[] args) {
         int initial = 100;
         int payment = 3000;
-        int one_bonus = 100;
+        int oneBonus = 100;
         if (payment <= 1000) {
             int bonus = 0;
             System.out.println((bonus) + "-начислено бонусов");
             System.out.println((initial + payment + bonus) + "-итоговый счет");
         }
-if (payment > 1000) {
-    int bonus = payment / one_bonus;
-    System.out.println((bonus) + "-начислено бонусов");
-    System.out.println((initial + payment + bonus) + "-итоговый счет");
-}
+        if (payment > 1000) {
+            int bonus = payment / oneBonus;
+            System.out.println((bonus) + "-начислено бонусов");
+            System.out.println((initial + payment + bonus) + "-итоговый счет");
+        }
     }
 }
